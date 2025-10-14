e86c1574a18f3c067816f3a6239cf29b4ae61ef1
srolest <serafinroldanesteban@gmail.com>
2025-10-14 13:29:32 +0200

    ejercicio1

---
+++ 3./ejercicio1.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es
+#mayor de edad o no.
+edad = float(input("Introduce tu edad: "))
+if edad > 18:
+    print ("Eres mayor de edad")
+else:
+    print ("Eres menor de edad")
