82a85748192001914d010f60c9a9d12b5fcab684
srolest <serafinroldanesteban@gmail.com>
2025-10-14 14:04:30 +0200

    ejercicio4

---
+++ 3./ejercicio4.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si
+#es par o impar.
+numero = int(input("Introduce un numero entero: "))
+if numero % 2:
+    print ("El numero es impar")
+else:
+    print ("El numero es par")
