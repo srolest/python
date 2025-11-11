f8dbba23ee7174e314d998e0a0977601933f73f1
srolest <serafinroldanesteban@gmail.com>
2025-11-11 14:16:14 +0100

    ejercicio6

---
+++ 4.Bucles/ejercicio6.py
@@ -0,0 +1,10 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla
+#un triángulo rectángulo como el de más abajo, de altura el número introducido.
+#*
+#**
+#***
+#****
+#*****
+numero = int(input("Introduce un numero entero:"))
+for i in range(1,numero +1):
+    print ("*" * i)
