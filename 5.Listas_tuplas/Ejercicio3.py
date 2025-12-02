d3bb7314ef43c70cf458dcb74f70dd7fe28aab2a
srolest <serafinroldanesteban@gmail.com>
2025-12-02 14:41:47 +0100

    Ejercicio3

---
+++ 5.Listas_tuplas/Ejercicio3.py
@@ -0,0 +1,4 @@
+asignaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
+for asignatura in asignaturas:
+    nota = input(f"Nota que has sacado en {asignatura}: ")
+    print(f"En {asignatura} has sacado {nota}")
